d22301084ee9b414a60d88a94f7d4df17f9e9391
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-03-18 17:58:06 +0100

    some-view.jsx

---
+++ frontend/lib/components/some-view.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var SomeView = React.createClass({
+	render: function() {
+		return (
+			<div className="SomeView">
+				some-view component
+			</div>
+		);
+	}
+});
+
+module.exports = SomeView;

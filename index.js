cd885ade146fac44ce7f79d6f3ccdbdf91954cf9
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-18 19:00:27 +0100

    Forever

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ Sealious.ConfigManager.set_config("datastore_chip_name", "mongo")
 
 Sealious.ConfigManager.set_config(
 	"chip.channel.www_server", 
-	{connections: [{port: 8080}]}
+	{
+		connections: [
+		{
+			port: 8080
+		}
+		]
+	}
 );
 
 Sealious.init();
+
 
 require("./backend/field-types/session_state.js");
 require("./backend/resource-types/session.js");

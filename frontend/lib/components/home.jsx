e7eff86c78196dffae0952f3c779d56618ca521a
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-03-18 17:57:09 +0100

    Home.jsx

---
+++ frontend/lib/components/home.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className="Home">
+				home component
+			</div>
+		);
+	}
+});
+
+module.exports = Home;

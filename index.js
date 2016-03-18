818a5488362474c7355d2dd94fae29b4a484ca11
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-18 20:30:18 +0100

    Checkpoint

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ Sealious.ConfigManager.set_config(
 Sealious.init();
 
 
+var www_server = Sealious.ChipManager.get_chip("channel", "www_server");
+www_server.static_route(path.resolve(module.filename, "../front/public"), "");
+
 require("./backend/field-types/session_state.js");
 require("./backend/resource-types/session.js");
 

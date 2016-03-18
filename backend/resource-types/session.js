6accbcc53c9004c5cb187f031c733df4ce8d521e
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-18 18:39:03 +0100

    Create backend

---
+++ backend/resource-types/session.js
@@ -0,0 +1,16 @@
+var Sealious = require("sealious");
+
+var session = new Sealious.ResourceType({
+	name: "session",
+	fields: [
+		{name: "last_changed_timestamp", type: "timestamp"},
+		{name: "work_duration", type: "int"},
+		{name: "break_duration", type: "int"},
+		{name: "slug", type: "text"},
+		{name: "current_state", type: "session_state"}
+	],
+	access_strategy: "public"
+})
+
+
+module.exports = session;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Sealious = require("sealious");
 
+Sealious.ConfigManager.set_config("datastore_chip_name", "mongo")
+
 Sealious.init();
 
 Sealious.start();

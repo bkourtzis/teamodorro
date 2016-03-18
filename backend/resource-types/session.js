8fa977ac875262c78511471974b0e4d668f26abe
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-18 18:54:55 +0100

    port

--- backend/resource-types/session.js
+++ backend/resource-types/session.js
@@ -3,7 +3,7 @@ var Sealious = require("sealious");
 var session = new Sealious.ResourceType({
 	name: "session",
 	fields: [
-		{name: "last_changed_timestamp", type: "timestamp"},
+		{name: "last_changed_timestamp", type: "datetime"},
 		{name: "work_duration", type: "int"},
 		{name: "break_duration", type: "int"},
 		{name: "slug", type: "text"},
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var Sealious = require("sealious");
 
 Sealious.ConfigManager.set_config("datastore_chip_name", "mongo")
 
+Sealious.ConfigManager.set_config(
+	"chip.channel.www_server", 
+	{connections: [{port: 8080}]}
+);
+
 Sealious.init();
+
+require("./backend/field-types/session_state.js");
+require("./backend/resource-types/session.js");
 
 Sealious.start();

98907ecba26f2821d82a5cca4a5eb5d4759ae3b7
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-03-18 17:57:34 +0100

    second-view.jsx

---
+++ frontend/lib/components/second-view.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var SecondView = React.createClass({
+	render: function() {
+		return (
+			<div className="SecondView">
+				second-view component 
+			</div>
+		);
+	}
+});
+
+module.exports = SecondView;

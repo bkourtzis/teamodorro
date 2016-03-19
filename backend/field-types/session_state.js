9531c824b83322604035949eb7ab58cd7a20440b
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-03-19 16:46:02 +0100

    checkpoint

--- backend/field-types/session_state.js
+++ backend/field-types/session_state.js
@@ -7,7 +7,7 @@ var session_state = new Sealious.FieldType({
 	name: "session_state",
 	is_proper_value: function(accept, reject, context, params, value_in_code){
 		if(allowed_values.indexOf(value_in_code) === -1){
-			reject(`Only these values are accepted: ${allowed_values.map((e)=>'"'+e+'"').join(", ")}.`);
+			reject(`Only these values are accepted: ${allowed_values.map((e)=>'"'+e+'"').join(", ")}. GOT: ${value_in_code}`);
 		}else{
 			accept();
 		}
